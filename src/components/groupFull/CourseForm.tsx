36516bb4ee2bd7bda9585855088eee15d4be3b54
Cофья Гергет <146007213+sslonixxx@users.noreply.github.com>
2025-02-21 18:08:48 +0700

    add status of course

--- src/components/groupFull/CourseForm.tsx
+++ src/components/groupFull/CourseForm.tsx
@@ -1,24 +1,41 @@
 import styles from "../../cssModuls/course.module.css";
 
 import Course from "./Course";
+import Status from "./Status";
 
 interface CourseFormProps {
   course: Course;
 }
 
 const CourseForm: React.FC<CourseFormProps> = ({ course }) => {
+  const status = Status[course.status as keyof typeof Status];
+  const statusClass =
+    {
+      Created: styles.statusCreated,
+      Started: styles.statusStarted,
+      OpenForAssigning: styles.statusOpenForAssigning,
+      Finished: styles.statusFinished,
+    }[course.status as keyof typeof Status] || "";
+
   return (
     <>
       <div className={styles.course}>
-        <h3>{course.name}</h3>
-        <div className={styles.groupName}>Учебный год - {course.startYear}</div>
-        <div className={styles.groupName}>Семестр - {course.semester}</div>
-        <span className={styles.smallFont}>
-          Мест всего - {course.maximumStudentsCount}
-        </span>
-        <span className={styles.smallFont}>
-          Мест свободно - {course.remainingSlotsCount}
-        </span>
+        <div className={styles.courseText}>
+          <div className={styles.container}>
+            <h3>{course.name}</h3>
+            <div className={`${styles.groupName} ${statusClass}`}>{status}</div>
+          </div>
+          <div className={styles.groupName}>
+            Учебный год - {course.startYear}
+          </div>
+          <div className={styles.groupName}>Семестр - {course.semester}</div>
+          <span className={styles.smallFont}>
+            Мест всего - {course.maximumStudentsCount}
+          </span>
+          <span className={styles.smallFont}>
+            Мест свободно - {course.remainingSlotsCount}
+          </span>
+        </div>
       </div>
     </>
   );
---
+++ src/components/groupFull/Status.ts
@@ -0,0 +1,8 @@
+const Status = {
+  Created: "Создан",
+  Started: "Открыт для записи",
+  OpenForAssigning: "В процессе обучения",
+  Finished: "Закрыт",
+};
+
+export default Status;
